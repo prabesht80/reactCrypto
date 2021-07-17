af05208bb7fc099f30e8f1fe14d2bde946c71280
prabesht80 <prabesht80@gmail.com>
2021-07-17 19:27:09 +0545

    layout changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Home from './component/pages/Home';
-// import requests from './component/api/NewsRequest';
+import { Switch, Route } from 'react-router-dom';
+import Layout from './component/pages/Layout';
 
 function App() {
-  // const [selectedOption, setSelectedOption] = useState(requests.Bitcoin);
   return (
     <div className="App">
-      <Home
-      // setSelectedOption={setSelectedOption}
-      // selectedOption={selectedOption}
-      />
+      <Layout>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
--- src/component/pages/Home.js
+++ src/component/pages/Home.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import AllCards from '../AllCards';
-import Footer from '../Footer';
 import Hero from '../Hero';
-import Navbar from '../Navbar';
 import NewsSection from '../NewsSection';
 
 function Home() {
   return (
     <>
-      <Navbar />
       <Hero />
       <AllCards />
       <NewsSection />
-      <Footer />
     </>
   );
 }
